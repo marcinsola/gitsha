b57c2f24d36f5ed03b79291693e93700385cb6d6
Marcin Sola <marcinsolaonline@gmail.com>
2020-05-02 15:42:51 +0200

    Parsing API response

--- gitsha.php
+++ gitsha.php
@@ -2,7 +2,7 @@
 
 class Gitsha {
     private array $arguments;
-    private array $baseUrls;
+    private array $services;
 
     public function __construct(array $arguments)
     {
@@ -12,30 +12,32 @@ class Gitsha {
                 'baseEndpoint' => 'https://api.github.com/repos/%s/commits',
                 'headers' => ['User-Agent: marcinsola'],
             ],
-        ]
+        ];
     }
 
     public function run(): string
     {
-        if (size($this->arguments) <= 1) {
+        if (sizeof($this->arguments) <= 1) {
             return 'Please provide repository name that you want to scan as an argument.';
         }
 
-        $service = isset($this->arguments[3] ? $this->arguments[3] : 'github';
+        $service = isset($this->arguments[3]) ? $this->arguments[3] : 'github';
         
         if (!in_array($service, array_keys($this->services))) {
             return sprintf('Unknown service: %s', $service);
         }
 
-        $endpoint = call_user_func(sprintf('prepare%sEndpoint', ucfirst($service));
-        //@TODO: Zaimplementować parseApiResponse
-        $result = $this->parseApiReponse(
+        //@TODO: zaimplementować ogólną metodę prepareEndpoint
+        $endpoint = $this->prepareGithubEndpoint();
+
+        return $this->parseApiResponse(
             $this->getApiResponse($endpoint, $service),
             $service
         );
     }
-    
-    //Tutaj przy bardziej rozbudowanej liczbie serwisów można by było zastosować fabrykę.
+
+    //Tutaj przy bardziej rozbudowanej liczbie serwisów można by było zastosować fabrykę
+    //i dla każdego z serwisów tworzyć osobne klasy (np. GithubProcessor, BitbucketProcessor, itp.).
     //Na potrzeby bierzącego zadania rozwiązałem to jednak w nieco prostszy sposób.
     private function prepareGithubEndpoint(): string
     {
@@ -47,51 +49,36 @@ class Gitsha {
        return $endpoint;
     }
 
-    private function getApiResult(string $endpoint, string $service): string
+    private function getApiResponse(string $endpoint, string $service): string
     {
+        $ch = curl_init();
         curl_setopt_array(
             $ch,
             [
-                CURLOPT_HTTPHEADER => [
-                    $this->services[$service]['headers'],
-                ],
+                CURLOPT_HTTPHEADER => $this->services[$service]['headers'] ?? [],
                 CURLOPT_URL => $endpoint,
                 CURLOPT_RETURNTRANSFER => true,
             ]
         );
+        
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-        return curl_exec($ch);
+        return $result;
+    }
+
+    private function parseApiResponse(string $response, string $service): string
+    {
+        $result = json_decode($response);
+
+        if (is_array($result)) {
+            return $result[0]->sha;
+        }
+
+        return  sprintf("Couldn't find repo %s. Please make sure that the repo and branch names provided are valid.", $this->arguments[1]);
     }
 }
 
-if ($argc <= 1) {
-    echo 'Please provide repository name that you want to scan as an argument.';
-    die;
-}
-$ch = curl_init();
-$endpoint = sprintf('https://api.github.com/repos/%s/commits', $argv[1]);
+$command = new Gitsha($argv);
+echo $command->run();
 
-if (isset($argv[2])) {
-    $endpoint .= sprintf('?sha=%s', $argv[2]);
-}
-
-curl_setopt_array(
-    $ch,
-    [
-        CURLOPT_HTTPHEADER => [
-            'User-Agent: marcinsola',
-        ],
-        CURLOPT_URL => $endpoint,
-        CURLOPT_RETURNTRANSFER => true,
-    ]
-);
-
-$result = json_decode(curl_exec($ch));
-curl_close($ch);
-
-if (is_array($result)) {
-    echo $result[0]->sha;
-    die;
-}
-
-echo sprintf("Couldn't find repo %s. Please make sure that the repo and branch names provided are valid.", $argv[1]);
